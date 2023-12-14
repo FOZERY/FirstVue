66d328d427707404eccf89f783494e176b0ffc2f
fozery <fozery@yandex.ru>
2023-12-15 01:52:45 +0300

    add: add delete-post and add-post buttons

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -3,7 +3,7 @@
     <div><strong>Название:</strong> {{ post.title }}</div>
     <div><strong>Описание:</strong> {{ post.body }}</div>
     <div class="post__btns">
-      <my-button>Удалить</my-button>
+      <my-button @click="$emit('remove', post)">Удалить</my-button>
     </div>
   </div>
 </template>
