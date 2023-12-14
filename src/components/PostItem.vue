81ed3b40eee9d26b236231641fa025aa44785aa9
fozery <fozery@yandex.ru>
2023-12-14 15:06:06 +0300

    add: new

---
+++ src/components/PostItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+    <div class="post__btns">
+      <my-button>Удалить</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  margin-top: 15px;
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/PostLists.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Список пользователей</h3>
+    <post-item v-for="post in posts" :post="post"></post-item>
+  </div>
+</template>
+
+<script>
+import PostItem from './PostItem.vue';
+
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: { PostItem },
+};
+</script>
+
+<style scoped></style>
